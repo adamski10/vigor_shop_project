c5e10756ce255d30b5c049fc704b75adbd7c70b9
AdamK <adamkoziel@outlook.com>
2020-06-06 10:39:26 +0100

    type changed to category

--- models/product.rb
+++ models/product.rb
@@ -2,13 +2,13 @@ require_relative('../db/sql_runner.rb')
 
 class Product
 
-  attr_reader :id, :manufacturer_id, :name, :type, :wholesale_price, :markup, :stock_level
+  attr_reader :id, :manufacturer_id, :name, :category, :wholesale_price, :markup, :stock_level
 
   def initialize(options)
     @id = options['id'] if options['id']
     @manufacturer_id = options['manufacturer.id']
     @name = options['name']
-    @category = options['type']
+    @category = options['category']
     @wholesale_price = options['wholesale_price']
     @markup = options['markup']
     @stock_level = options['stock_level']
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -18,4 +18,9 @@ class TestProduct < MiniTest::Test
     assert_equal("Organic flour", result)
   end
 
+  def test_category()
+    result = @organic_flour.category()
+    assert_equal("flour", result)
+  end
+
 end
